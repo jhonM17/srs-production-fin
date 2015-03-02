22a29a0147a98cf03920b9fce65df0efc84f2a6b
unknown <jhofe_s17@hotmail.com>
2015-03-02 16:16:35 -0500

    todoo

--- srs/settings/base.py
+++ srs/settings/base.py
@@ -18,7 +18,7 @@ DJANGO_APPS = (
             'django.contrib.sessions',
             'django.contrib.messages',
             'django.contrib.staticfiles',
-            'multiselectfield',
+
         )
 
 THIRD_PARTY_APPS = (
